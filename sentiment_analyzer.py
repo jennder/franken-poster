7624d60d062f69ec8cfb49df6570d2993d9abcee
felicia <zhang.fel@northeastern.edu>
2021-04-12 19:39:01 -0400

    corpus + start sentiment

---
+++ sentiment_analyzer.py
@@ -0,0 +1,27 @@
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+"""
+Performs sentiment analysis on a movie based on its dialogues
+and can create a movie poster background based on the sentiment
+of the conversations.
+"""
+class SentimentAnalyzer:
+    def __init__(self, movie_id):
+        pass
+    
+    def run_model(self):
+        pass
+
+"""
+Using nltk's sentiment analyzer
+"""
+class VaderAnalyzer(SentimentAnalyzer):
+    def run_model(self):
+        pass
+
+"""
+Using TextBlob's Naive Bayes sentiment analyzer
+"""
+class NaiveAnalyzer(SentimentAnalyzer):
+    def run_model(self):
+        pass
