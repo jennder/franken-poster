57b304b90815fa0dc16a9b41885159af9b691a46
jennder <der.je@husky.neu.edu>
2021-04-15 17:44:33 -0400

    update deprecated code

--- poem.py
+++ poem.py
@@ -41,7 +41,7 @@ class PoemGen:
             tokenizer, max_sequence_len = self.model_setup()
             token_list = tokenizer.texts_to_sequences([input_phrase])[0]
             token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre') 
-            predicted = model.predict_classes(token_list, verbose=0)
+            predicted = np.argmax(model.predict(token_list), axis=-1)
             output_word = ""
             for word, index in tokenizer.word_index.items():
                 if index == predicted:
